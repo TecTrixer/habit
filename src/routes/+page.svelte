d16459ba5b80ac505d7ee1c02edad64a143c33fa
TecTrixer <tonihoevedes@gmail.com>
2023-03-02 21:18:35 +0100

    removed unused stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,3 @@
-<script lang="ts">
-  import { onMount } from "svelte";
-  import Countdown from "$lib/Countdown.svelte";
-
-  let until: Date | null;
-  onMount(() => {
-    until = new Date(new Date().getTime() + 1000 * 125);
-    setTimeout(() => {
-      until = new Date(new Date().getTime() + 1000 * 65);
-    }, 5500);
-  });
-
-  function get_data() {}
-
-  function tick() {}
-</script>
-
 <div>
-  <Countdown {until} />
-
-  <button
-    on:mousedown={tick}
-    class="btn-primary btn flex mt-10 btn-square mx-auto">TICK</button
-  >
+  <h1 class="text-center text-5xl my-16">Habify</h1>
 </div>
