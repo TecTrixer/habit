26b47949ea034272eeb5f5dd3a93ba912c5de0fc
TecTrixer <tonihoevedes@gmail.com>
2023-03-02 22:44:47 +0100

    created db models + mvp oauth with github

--- pb/main.go
+++ pb/main.go
@@ -2,15 +2,24 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	app.OnRecordBeforeCreateRequest().Add(func(e *core.RecordCreateEvent) error {
+		log.Println(e.Record) // still unsaved
+		user := e.Record.Get("user")
+		log.Println("userID:", user)
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
